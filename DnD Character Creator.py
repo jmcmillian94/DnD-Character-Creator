1c3a1523e0f22933b77c92f6b931a8fb85d350c8
unknown <jmcmillian94@aim.com>
2024-08-18 14:37:29 -0400

    update

--- DnD Character Creator.py
+++ DnD Character Creator.py
@@ -5,14 +5,30 @@ root = Tk()
 root.title("DnD Character Creator")
 root.geometry("800x600")
 
-#race selection
-Label(root, text='Select Your Race', wraplength=180, justify="left", anchor="nw").grid(row=0, column=0, padx=10, pady=10, sticky="nw")
+leftFrame = Frame(root)
+leftFrame.grid(row=0, column=0, padx=10, pady=10, sticky="ns")
 
-raceListbox = Listbox(root, selectmode=SINGLE, exportselection=False)
+rightFrame = Frame(root)
+rightFrame.grid(row=0, column=1, padx=10, pady=10, sticky="ns")
+
+#initalize character info
+character = {'Name': 'N/A',
+             'Race': 'N/A',
+             'Sex': 'N/A',
+             'Level': '1', 
+             'Class': 'N/A',
+             }
+character_info_var = StringVar()
+character_info_var.set("\n".join([f"{key}: {value}" for key, value in character.items()]))
+characterInfoLabel = Label(rightFrame, textvariable=character_info_var, anchor="nw", justify="left")
+characterInfoLabel.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
+
+#race selection
+Label(leftFrame, text='Select Your Race', wraplength=180, justify="left", anchor="nw").grid(row=0, column=0, padx=10, pady=10, sticky="nw")
+raceListbox = Listbox(leftFrame, selectmode=SINGLE, exportselection=False)
 raceListbox.grid(row=1, column=0, padx=10, pady=10)
 for race in listOfRaces:
         raceListbox.insert(END, race)
-
-Button(root,text='Select Race').grid(row=2, column=0, padx=10, pady=10, sticky="nw")
+Button(leftFrame,text='Select Race').grid(row=2, column=0, padx=10, pady=10, sticky="nw")
 
 root.mainloop()
